31c6dbd9e14907f94ac4054043e7ee3555dc98a2
Victor G. Marques <victormarques8801@gmail.com>
2021-12-15 20:44:39 -0300

    Cria função para converter texto em áudio

--- texto_para_fala.py
+++ texto_para_fala.py
@@ -2,3 +2,8 @@ from gtts import gTTS
 from playsound import playsound
 
 
+def texto_para_fala(texto):
+    print('Conversão iniciada...')
+    audio = gTTS(texto, lang='pt')
+    audio.save('audio_python.mp3')
+    playsound('audio_python.mp3')
